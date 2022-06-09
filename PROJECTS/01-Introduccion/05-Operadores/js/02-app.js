b393c0127e28499123922773b01b0126489d1b77
Florencia Lema <flor.lema222@gmail.com>
2022-06-09 11:24:02 -0300

    booleans and ternary review

---
+++ PROJECTS/01-Introduccion/05-Operadores/js/02-app.js
@@ -0,0 +1,10 @@
+let numero;
+console.log(numero); //undefined
+console.log(typeof numero); //undefined
+
+let numero2 = null;
+console.log(numero2); //null
+console.log(typeof numero2); //object, null is an object
+
+console.log(numero === numero2); //false
+console.log(numero == numero2); //true
---
+++ PROJECTS/01-Introduccion/06-Booleans/js/02-app.js
@@ -0,0 +1,12 @@
+const autenticado = true;
+
+
+//forma larga del if
+/* if (autenticado) {
+  console.log("puedes ver netflix");
+} else {
+  console.log("no puedes ver netflix");
+} */
+
+//ternario
+console.log( autenticado ? "puedes ver netflix!" : "no puedes ver netflflix");
